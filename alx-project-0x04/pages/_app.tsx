1741780d2d7e38ffa5eb56ba8bd0dd605c61028c
Njeri Kamau <faithkamau730@gmail.com>
2025-06-14 16:22:56 +0300

    app.tsx adjustment

--- alx-project-0x04/pages/_app.tsx
+++ alx-project-0x04/pages/_app.tsx
@@ -1,11 +1,6 @@
-import { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
-import store from '../store';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Provider store={store}>
-    <Component {...pageProps} />
-  </Provider>
-);
-
-export default MyApp;
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
